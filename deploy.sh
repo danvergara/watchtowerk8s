4dd47d137f61c28c240035951b182f156db879e0
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-05-17 15:20:21 -0500

    docs(Doc-files): improve the documetation files

--- deploy.sh
+++ deploy.sh
@@ -1,41 +1,16 @@
 #!/bin/bash
 
-#
-# echo "Creating the database credentials..."
-#
-# kubectl apply -f ./kubernetes/secret.yml
-
-echo "Installing SealedSecret CRD, server side controller into kube-system namespace..."
-
-kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.12.1/controller.yaml
-
-
-echo "Encrypting the secrets..."
-
-kubeseal <kubernetes/secrets.json>kubernetes/sealedsecrets.json
-
-
-echo "Creating the sealed secret..."
-
-kubectl create -f  kubernetes/sealedsecrets.json
+echo "Creating the database credentials..."
+kubectl apply -f ./kubernetes/secret.yml
 
 echo "Creating the dashboardserver deployment and service..."
-
-kubectl create -f ./kubernetes/dashboardserver-deployment.yml
-kubectl create -f ./kubernetes/dashboardserver-service.yml
-
-
-echo "Adding the ingress..."
-
-minikube addons enable ingress
-kubectl apply -f ./kubernetes/dashboard-ingress.yml
-
+kubectl apply -f ./kubernetes/dashboardserver.yml
 
 echo "Creating the vue deployment and service..."
+kubectl apply -f ./kubernetes/dashboard.yml
 
-kubectl create -f ./kubernetes/dashboard-deployment.yml
-kubectl create -f ./kubernetes/dashboard-service.yml
-
-
-echo "Retrieving the ip of the local cluster"
-echo "$(minikube ip) dashboard.soydanielvergara.com" | sudo tee -a /etc/hosts
+echo "Adding the ingress..."
+kubectl apply -f ./kubernetes/prod-issuer.yaml
+kubectl apply -f ./kubernetes/staging-issuer.yaml
+kubectl apply -f ./kubernetes/watchtower-ingress.yml
+kubectl apply -f ./kubernetes/cloud-generic.yaml
